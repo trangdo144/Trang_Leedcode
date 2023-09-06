61fa8310378bebfd3cafd2c0513979013eab1db1
trangdo144 <117735432+trangdo144@users.noreply.github.com>
2023-09-06 11:30:20 +1000

    intersection

---
+++ Leedcode/intersection.js
@@ -0,0 +1,9 @@
+var intersection = function (nums1, nums2) {
+  let intersectionList = [];
+  nums1.forEach((num1) => {
+    if (nums2.includes(num1) & !intersectionList.includes(num1)) {
+      intersectionList.push(num1);
+    }
+  });
+  return intersectionList;
+};
