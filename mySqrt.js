842a2dfb3eb760daa61ecb085ac490eb4ec396df
trangdo144 <117735432+trangdo144@users.noreply.github.com>
2023-10-04 13:42:45 +1100

    my sqrt

--- mySqrt.js
+++ mySqrt.js
@@ -2,18 +2,13 @@
 https: var mySqrt = function (x) {
   let left = 0;
   let right = x;
-  while (left < right) {
+  while (left < right - 1) {
     let mid = left + Math.trunc((right - left) / 2);
-    if (Math.pow(mid) === x) return mid;
     if (Math.pow(mid) > x) {
-      right = mid - 1;
+      right = mid;
     } else {
-      left = mid + 1;
+      left = mid;
     }
   }
-  if (Math.pow(left) > x) {
-    return left - 1;
-  } else {
-    return left;
-  }
+  return left;
 };
