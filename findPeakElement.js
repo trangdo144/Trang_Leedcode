d6f0af1a460b6eb46e67a770a6a411c4ecad2acb
trangdo144 <117735432+trangdo144@users.noreply.github.com>
2023-10-18 13:16:48 +1100

    find peak element

---
+++ findPeakElement.js
@@ -0,0 +1,25 @@
+var findPeakElement = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  if (nums[left] > nums[left + 1]) {
+    return left;
+  }
+  if (nums[right] > nums[right - 1]) {
+    return right;
+  }
+
+  while (left < right - 1) {
+    let mid = left + Math.trunc((right - left) / 2);
+    if (nums[mid] < nums[mid - 1]) {
+      right = mid;
+    }
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid;
+    }
+    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+      left = mid;
+    }
+  }
+  return left;
+};
